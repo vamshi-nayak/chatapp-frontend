1ebf9800b90cdd79e1dc12d53bddceff2fb08855
vamshi nayak <136154466+vamshi-nayak@users.noreply.github.com>
2023-09-18 10:44:25 +0530

    Update Login.js

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -55,7 +55,7 @@ const Login = () => {
 
       localStorage.setItem("userInformation", JSON.stringify(data));
       setLoading(false);
-      navigate("/ChatBox");
+      navigate("/HomePage");
     } catch (error) {
       toast({
         title: "Error Occured!",
